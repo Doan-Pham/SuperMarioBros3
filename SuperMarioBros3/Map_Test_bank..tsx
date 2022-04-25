5f220c9de0bf0fced3718b909c5879e9c6a21a53
Doan Pham <Doan-Pham@users.noreply.github.com>
2022-04-25 08:48:54 +0700

    Add .png and .xml files for World 1-1 map

---
+++ SuperMarioBros3/Map_Test_bank..tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.8" tiledversion="1.8.4" name="Map_Test_bank.png" tilewidth="16" tileheight="16" tilecount="121" columns="11">
+ <image source="Map_Test_bank.png" width="176" height="176"/>
+</tileset>
