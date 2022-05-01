448f3b47118339ab996c864360ffad1516bd3373
Doan Pham <Doan-Pham@users.noreply.github.com>
2022-05-01 20:32:37 +0700

    Manage to make brick_questionmark bounce up a bit then fall
    TODO: The brick doesn't stop at the orignal position

--- SuperMarioBros3/BrickQuestionMark.cpp
+++ SuperMarioBros3/BrickQuestionMark.cpp
@@ -4,7 +4,7 @@ void CBrickQuestionMark::Render()
 {
 	CAnimations* animations = CAnimations::GetInstance();
 	if (!isContentTaken)
-	animations->Get(ID_ANI_BRICK_QUESTIONMARK)->Render(x, y);
+		animations->Get(ID_ANI_BRICK_QUESTIONMARK)->Render(x, y);
 	else
 		animations->Get(ID_ANI_TEST)->Render(x, y);
 	//RenderBoundingBox();
@@ -12,9 +12,20 @@ void CBrickQuestionMark::Render()
 
 void CBrickQuestionMark::Update(DWORD dt, vector<LPGAMEOBJECT>* coObjects)
 {
-	if (isContentTaken) return;
-	DebugOutTitle(L"Brick Question Mark state is: %d \n",
-		isContentTaken);
+
+	vy += ay * dt;
+
+	if (y + vy*dt != y_original)
+	{
+		vy = y_original - y;
+	}
+	//y += vy * dt;
+	DebugOutTitle(L"Brick Question Mark y: %0.5f, vy: %0.5f, ay: %0.5f  \n",
+		y, vy, ay);
+	//if (abs(vx) > abs(maxVx)) vx = maxVx;
+	//if (isContentTaken) return;
+	//DebugOutTitle(L"Brick Question Mark state is: %d \n",
+	//	isContentTaken);
 	//vy += ay * dt;
 	CCollision::GetInstance()->Process(this, dt, coObjects);
 }
@@ -24,17 +35,21 @@ void CBrickQuestionMark::OnCollisionWith(LPCOLLISIONEVENT e)
 {
 	if (isContentTaken) return;
 	if (!(dynamic_cast<CMario*>(e->obj))) return;
-	//if (e->ny > 0)
-	//this->vy = -BRICK_QUESTION_MARK_BOUNCE_SPEED;
+	if (e->ny > 0 && e->nx == 0)
+	{
+		this->vy = -BRICK_QUESTION_MARK_BOUNCE_SPEED;
+		this->ay = BRICK_QUESTION_MARK_GRAVITY;
+	}
+		
 	isContentTaken = true;
-	DebugOutTitle(L"Brick Question Mark state is: %d \n", 
-		isContentTaken );
+	DebugOutTitle(L"Brick Question Mark state is: %d \n",
+		isContentTaken);
 }
 
 void CBrickQuestionMark::OnNoCollision(DWORD dt)
 {
 	//if (isContentTaken) return;
-	//y += vy * dt;
+	y += vy * dt;
 }
 
 
--- SuperMarioBros3/BrickQuestionMark.h
+++ SuperMarioBros3/BrickQuestionMark.h
@@ -6,16 +6,19 @@
 
 #define ID_ANI_BRICK_QUESTIONMARK 10000
 #define ID_ANI_TEST 11000
-#define BRICK_QUESTION_MARK_GRAVITY 0.0002f
-#define BRICK_QUESTION_MARK_BOUNCE_SPEED 0.1f
+#define BRICK_QUESTION_MARK_GRAVITY 0.002f
+#define BRICK_QUESTION_MARK_BOUNCE_SPEED 0.4f
 class CBrickQuestionMark : public CBrick
 {
 protected:
 	bool isContentTaken;
+	float y_original;
 	float ay;
 public:
 	CBrickQuestionMark(float x, float y) : CBrick(x, y) 
-	{ isContentTaken = false; ay = 0; }
+	{
+		isContentTaken = false; ay = 0; y_original = this->y;
+	}
 	virtual void Render();
 	virtual void Update(DWORD dt, vector<LPGAMEOBJECT>* coObjects);
 	virtual void OnCollisionWith(LPCOLLISIONEVENT e);
