d9366d7c37e2fedb14bbd340773a650eb8f3f124
Doan Pham <Doan-Pham@users.noreply.github.com>
2022-05-01 20:45:59 +0700

    Make brick_questionmark fully stops at original position after bouncing
    Solution: Make vy = 0 when the brick arrives at the original position
    
    TODO:
    - Incorportate this logic better with the "isContentTaken" flag to avoid having to process collisions.
    - Only collisions that happen on the top of the brick are detected (already test with e->ny < 0, collisions in that situation seem to never happen).
        Cause: Possibly because at the same time, program is also checking collisions for mario, and something broke along the way
        Solution (Not tested): Leave the collisions processing to mario and also at that time, set the brick's state. In CBrickQuestionMark, use states to update

--- SuperMarioBros3/BrickQuestionMark.cpp
+++ SuperMarioBros3/BrickQuestionMark.cpp
@@ -20,6 +20,7 @@ void CBrickQuestionMark::Update(DWORD dt, vector<LPGAMEOBJECT>* coObjects)
 		vy = (y_original - y)/dt;
 	}
 	y += vy * dt;
+	vy = 0;
 	DebugOutTitle(L"Brick Question Mark y: %0.5f, vy: %0.5f, ay: %0.5f  \n",
 		y, vy, ay);
 	//if (abs(vx) > abs(maxVx)) vx = maxVx;
