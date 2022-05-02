656b2f0c0ecfa3d15a59c819b330c7dec336bca5
Doan Pham <Doan-Pham@users.noreply.github.com>
2022-05-02 07:32:49 +0700

    Smooth rendering of tilelayers
    Solution: Calculate the positions of the tiles that are within the camera and the max tiles there can be on the window. Then only render those tiles
    
    TODO: The numbers are still hard-coded, and CPU usage didn't change much

--- SuperMarioBros3/TileLayer.h
+++ SuperMarioBros3/TileLayer.h
@@ -4,7 +4,7 @@
 #include <vector>
 
 #include "TileSet.h"
-
+#include "Game.h"
 
 class CTileLayer
 {
@@ -37,10 +37,15 @@ public:
 	void Render() 
 	{
 		int tileWidth, tileHeight = 0;
-
-		for (int i = 0; i < height; i++)
+		float camX, camY = 0.0f;
+		CGame::GetInstance()->GetCamPos(camX, camY);
+		int currentPositionX = camX / 16;
+		int currentPositionY = camY / 16;
+		int maxTilesX = 320 / 16;
+		int maxTilesY = 240 / 16;
+		for (int i = currentPositionY; i < min(height,i + maxTilesY); i++)
 		{
-			for (int j = 0; j < width; j++)
+			for (int j = currentPositionX; j < min(width,j + maxTilesX); j++)
 			{
 				int tileGid = tileMatrix[i][j];
 				int usedTileSetIndex = tileGid / (tileSets[0]->GetLastGid() + 1);
@@ -55,6 +60,9 @@ public:
 				//	, i, j,tileMatrix[i][j]);
 			}
 		}
+
+		DebugOutTitle(L"camX: %0.5f, camY : %0.5f, currentPositionX : %0.5f, currentPositionY : %0.5f",
+			camX, camY, currentPositionX, currentPositionY);
 	};
 
 	void Clear()
