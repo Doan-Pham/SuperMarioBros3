6724579e2502051ed221d3fccd487fe6782b4f31
Doan Pham <Doan-Pham@users.noreply.github.com>
2022-05-03 12:20:17 +0700

    Add states and vector of hidden items to brick_questionmark

--- SuperMarioBros3/BrickQuestionMark.cpp
+++ SuperMarioBros3/BrickQuestionMark.cpp
@@ -1,9 +1,17 @@
 #include "BrickQuestionMark.h"
 
+CBrickQuestionMark::CBrickQuestionMark(float x, float y) : CBrick(x, y)
+{
+	y_original = this->y;
+	isHiddenItemAppeared = false;
+	SetState(BRICK_STATE_NORMAL);
+	
+}
+
 void CBrickQuestionMark::Render()
 {
 	CAnimations* animations = CAnimations::GetInstance();
-	if (!isHitByMario)
+	if (state == BRICK_STATE_NORMAL)
 		animations->Get(ID_ANI_BRICK_QUESTIONMARK_NORMAL)->Render(x, y);
 	else
 		animations->Get(ID_ANI_BRICK_QUESTIONMARK_HIT)->Render(x, y);
@@ -25,26 +33,54 @@ void CBrickQuestionMark::Update(DWORD dt, vector<LPGAMEOBJECT>* coObjects)
 
 	//DebugOutTitle(L"Brick Question Mark y: %0.5f, vy: %0.5f, ay: %0.5f  \n",
 	//	y, vy, ay);
-	if (isHitByMario) return;
+	if (state == BRICK_STATE_HIT_BY_MARIO)
+	{
+		if (y == y_original && !isHiddenItemAppeared)
+		{
+			if (hiddenItems.size() == 0)
+			{
+				CGame::GetInstance()->UpdateCoins(this->GetCoinsGivenWhenHit());
+				CGame::GetInstance()->UpdateScores(this->GetScoresGivenWhenHit());
+			}
+			else
+			{
+				for (int i = 0; i < hiddenItems.size(); i++)
+					hiddenItems[i]->SetState(MUSHROOM_STATE_APPEARING);
+			}
+			isHiddenItemAppeared = true;
+		}
+		return;
+	}
 	CCollision::GetInstance()->Process(this, dt, coObjects);
 }
 
 
 void CBrickQuestionMark::OnCollisionWith(LPCOLLISIONEVENT e)
 {
-	if (isHitByMario) return;
+	if (state == BRICK_STATE_HIT_BY_MARIO) return;
 	if (!(dynamic_cast<CMario*>(e->obj))) return;
 	if (e->ny < 0 && e->nx == 0)
 	{
-		this->vy = -BRICK_QUESTION_MARK_BOUNCE_SPEED;
-		this->ay = BRICK_QUESTION_MARK_GRAVITY;
-		isHitByMario = true;
-		CGame::GetInstance()->UpdateCoins(this->GetCoinsGivenWhenHit());
-		CGame::GetInstance()->UpdateScores(this->GetScoresGivenWhenHit());
+		SetState(BRICK_STATE_HIT_BY_MARIO);
 	}
 	//DebugOutTitle(L"Brick Question Mark state is: %d \n",
 	//	isContentTaken);
 }
 
+void CBrickQuestionMark::SetState(int state)
+{
+	CGameObject::SetState(state);
+	switch (state)
+	{
+	case BRICK_STATE_NORMAL:
+		ay = 0;
+		break;
+
+	case BRICK_STATE_HIT_BY_MARIO:
+		vy = -BRICK_QUESTION_MARK_BOUNCE_SPEED;
+		ay = BRICK_QUESTION_MARK_GRAVITY;
+		break;
+	}
+}
 
 
--- SuperMarioBros3/BrickQuestionMark.h
+++ SuperMarioBros3/BrickQuestionMark.h
@@ -3,30 +3,39 @@
 #include "Brick.h"
 #include "Mario.h"
 #include "debug.h"
+#include "MushroomBig.h"
 
 #define ID_ANI_BRICK_QUESTIONMARK_NORMAL 3111
 #define ID_ANI_BRICK_QUESTIONMARK_HIT 3112
+
 #define BRICK_QUESTION_MARK_GRAVITY 0.002f
 #define BRICK_QUESTION_MARK_BOUNCE_SPEED 0.3f
+
+#define BRICK_STATE_NORMAL 100
+#define BRICK_STATE_HIT_BY_MARIO 200
 
 class CBrickQuestionMark : public CBrick
 {
 protected:
-	bool isHitByMario;
 	float y_original;
 	float ay;
-public:
-	CBrickQuestionMark(float x, float y) : CBrick(x, y) 
-	{
-		isHitByMario = false; ay = 0; y_original = this->y;
-	}
+
+	bool isHiddenItemAppeared;
+	vector<CItem*> hiddenItems;
+
 	virtual void Render();
 	virtual void Update(DWORD dt, vector<LPGAMEOBJECT>* coObjects);
 	virtual void OnCollisionWith(LPCOLLISIONEVENT e);
 
 	virtual int IsCollidable() { return 1; };
 
+public:
+	CBrickQuestionMark(float x, float y);
+
 	virtual int GetScoresGivenWhenHit() { return 100; }
 	virtual int GetCoinsGivenWhenHit() { return 1; }
+
+	virtual void SetState(int state);
+	virtual void AddHiddenItem(CItem* item) { hiddenItems.push_back(item); }
 };
 
