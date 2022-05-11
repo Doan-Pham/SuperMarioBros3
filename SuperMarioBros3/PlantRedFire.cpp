c7021c2607f1b458610d1640e03153a19c779767
Doan Pham <Doan-Pham@users.noreply.github.com>
2022-05-11 13:36:38 +0700

    Add basic methods and info for plant_red_fire

--- SuperMarioBros3/PlantRedFire.cpp
+++ SuperMarioBros3/PlantRedFire.cpp
@@ -3,6 +3,7 @@
 
 CPlantRedFire::CPlantRedFire(float x, float y) : CGameObject(x,y)
 {
+	SetState(PLANT_STATE_HIDING);
 }
 
 void CPlantRedFire::GetBoundingBox(float& left, float& top, float& right, float& bottom)
@@ -15,17 +16,43 @@ void CPlantRedFire::GetBoundingBox(float& left, float& top, float& right, float&
 
 void CPlantRedFire::Update(DWORD dt, vector<LPGAMEOBJECT>* coObjects)
 {
+
 }
 
 void CPlantRedFire::Render()
 {
+	if (state == PLANT_STATE_HIDING) return;
+	int aniSpriId;
 	CAnimations::GetInstance()->Get(ID_ANI_PLANT_RED_FIRE_OPEN_CLOSE_TOP_LEFT)->Render(x, y);
 	RenderBoundingBox();
-	DebugOut(L"plant_x : %0.5f, plant_y : %0.5f \n", x, y);
+	//DebugOut(L"plant_x : %0.5f, plant_y : %0.5f \n", x, y);
 }
 
-
+int CPlantRedFire::GetPlantAniSpriId()
+{
+	int aniSprId;
+	if (this->state == PLANT_STATE_FIRING) aniSprId = ID_SPRITE_PLANT_RED_FIRE_OPEN_TOP_LEFT;
+	else aniSprId = ID_ANI_PLANT_RED_FIRE_OPEN_CLOSE_TOP_LEFT;
+}
 
 void CPlantRedFire::SetState(int state)
 {
+	CGameObject::SetState(state);
+	switch (state)
+	{
+	case PLANT_STATE_HIDING:
+		vy = 0;
+		break;
+
+	case PLANT_STATE_APPEARING:
+		vy = -PLANT_MOVING_SPEED;
+		break;
+
+	case PLANT_STATE_FIRING:
+		break;
+
+	case PLANT_STATE_DISAPPEARING:
+		vy = PLANT_MOVING_SPEED;
+		break;
+	}
 }
--- SuperMarioBros3/PlantRedFire.h
+++ SuperMarioBros3/PlantRedFire.h
@@ -3,14 +3,17 @@
 #include "GameObject.h"
 #include "debug.h"
 
+#define	PLANT_RENDER_PRIORITY	10
+
 #define PLANT_BBOX_WIDTH 16
 #define PLANT_BBOX_HEIGHT 32
 
+#define PLANT_MOVING_SPEED 0.02f
 
-#define ID_SPRITE_PLANT_RED_FIRE_OPEN_MOUTH_TOP_LEFT		53211
-#define ID_SPRITE_PLANT_RED_FIRE_OPEN_MOUTH_TOP_RIGHT		53221
-#define ID_SPRITE_PLANT_RED_FIRE_OPEN_MOUTH_BOTTOM_LEFT		53231
-#define ID_SPRITE_PLANT_RED_FIRE_OPEN_MOUTH_BOTTOM_RIGHT	53241
+#define ID_SPRITE_PLANT_RED_FIRE_OPEN_TOP_LEFT				53211
+#define ID_SPRITE_PLANT_RED_FIRE_OPEN_TOP_RIGHT				53221
+#define ID_SPRITE_PLANT_RED_FIRE_OPEN_BOTTOM_LEFT			53231
+#define ID_SPRITE_PLANT_RED_FIRE_OPEN_BOTTOM_RIGHT			53241
 
 #define ID_ANI_PLANT_RED_FIRE_OPEN_CLOSE_TOP_LEFT			5321
 #define ID_ANI_PLANT_RED_FIRE_OPEN_CLOSE_TOP_RIGHT			5322
@@ -19,21 +22,31 @@
 
 #define PLANT_SCORES_GIVEN_WHEN_HIT 100
 
+#define PLANT_STATE_HIDING			100
+#define PLANT_STATE_APPEARING		200
+#define PLANT_STATE_FIRING			300
+#define PLANT_STATE_DISAPPEARING	400
+
+
+
 class CPlantRedFire : public CGameObject
 {
 protected:
-	float ax;
-	float ay;
-
 	virtual void GetBoundingBox(float& left, float& top, float& right, float& bottom);
 	virtual void Update(DWORD dt, vector<LPGAMEOBJECT>* coObjects);
 	virtual void Render();
 
+	int GetPlantAniSpriId();
+
 	virtual int IsBlocking() { return 0; }
+	virtual bool IsHidden() { return (state == PLANT_STATE_HIDING); }
 
 public:
 	CPlantRedFire(float x, float y);
 	virtual void SetState(int state);
 	virtual int GetScoresGivenWhenHit() { return PLANT_SCORES_GIVEN_WHEN_HIT; }
+
+	// To be hidden by the pipe
+	virtual int GetRenderPriority() { return PLANT_RENDER_PRIORITY; }
 };
 
