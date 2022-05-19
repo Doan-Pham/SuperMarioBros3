1db121d6d3e11956330c6f231f0ad88672919c63
Doan Pham <Doan-Pham@users.noreply.github.com>
2022-05-19 12:48:05 +0700

    Create CKoopa - base class for all koopas

---
+++ SuperMarioBros3/Koopa.h
@@ -0,0 +1,80 @@
+#pragma once
+
+#include "GameObject.h"
+#include "debug.h"
+#include "AttachedBBox.h"
+#include "PlayScene.h"
+
+
+#define KOOPA_NORMAL_BBOX_WIDTH		16
+#define KOOPA_NORMAL_BBOX_HEIGHT	26
+
+#define KOOPA_SHELL_BBOX_WIDTH		16
+#define KOOPA_SHELL_BBOX_HEIGHT		16
+
+#define KOOPA_SCORES_GIVEN_WHEN_HIT 100
+
+
+#define KOOPA_GRAVITY 0.002f
+#define KOOPA_WALKING_SPEED 0.05f
+#define KOOPA_SHELL_MOVING_SPEED	0.15f
+
+#define KOOPA_STATE_WALKING								100
+
+#define KOOPA_STATE_SHELL_DOWNSIDE_STILL				200
+#define KOOPA_STATE_SHELL_DOWNSIDE_MARIO_HOLD			300
+#define KOOPA_STATE_SHELL_DOWNSIDE_MOVING				400
+
+#define KOOPA_STATE_SHELL_UPSIDE_STILL					500
+#define KOOPA_STATE_SHELL_UPSIDE_MARIO_HOLD				600
+#define KOOPA_STATE_SHELL_UPSIDE_MOVING					700
+
+#define KOOPA_STATE_DIE									800
+
+#define KOOPA_SHELL_TIMEOUT	9000
+#define BLOCK_PUSH_FACTOR_GHOST_PLATFORM 1.0f
+
+class CPlayScene;
+typedef CPlayScene* LPPLAYSCENE;
+
+class CKoopa : public CGameObject
+{
+protected:
+
+	float ay;
+
+	CAttachedBBox* attachedBBox;
+
+	BOOLEAN isShell;
+	BOOLEAN isBeingHeld;
+
+	ULONGLONG shell_start;
+	// *** CONST *** pointer to the current playscene
+	const LPPLAYSCENE currentScene;
+
+	virtual void GetBoundingBox(float& left, float& top, float& right, float& bottom);
+	virtual void Update(DWORD dt, vector<LPGAMEOBJECT>* coObjects);
+	virtual int GetAniId() = 0;
+	virtual void Render() = 0;
+
+	virtual int IsCollidable() { return 1; };
+	virtual int IsBlocking() { return 0; }
+	virtual void OnNoCollision(DWORD dt);
+	virtual void OnCollisionWith(LPCOLLISIONEVENT e);
+
+	void OnCollisionWithPlatformGhost(LPCOLLISIONEVENT e);
+
+	void OnCollisionWithPlantRedFire(LPCOLLISIONEVENT e);
+
+	void OnCollisionWithGoomba(LPCOLLISIONEVENT e);
+
+	void OnCollisionWithBrickQuestionMark(LPCOLLISIONEVENT e);
+
+
+public:
+	CKoopa(float x, float y, const LPPLAYSCENE& currentScene);
+	void SetDirection(int nx) { this->nx = nx; }
+	virtual void SetState(int state);
+	virtual int GetScoresGivenWhenHit() { return KOOPA_SCORES_GIVEN_WHEN_HIT; }
+};
+
--- SuperMarioBros3/KoopaRedNormal.cpp
+++ SuperMarioBros3/KoopaRedNormal.cpp
@@ -14,40 +14,15 @@
 #include "Leaf.h"
 
 CKoopaRedNormal::CKoopaRedNormal(float x, float y, const LPPLAYSCENE& currentScene)
-	:CGameObject(x, y), currentScene(currentScene)
+	:CKoopa(x, y, currentScene)
 {
-	isShell = false;
-	isBeingHeld = false;
-	shell_start = -1;
-	nx = -1;
-	this->ay = KOOPA_GRAVITY;
-	SetState(KOOPA_STATE_WALKING);
-}
-
-void CKoopaRedNormal::GetBoundingBox(float& left, float& top, float& right, float& bottom)
-{
-	if (state == KOOPA_STATE_WALKING)
-	{
-		left = x - KOOPA_NORMAL_BBOX_WIDTH / 2;
-		top = y - KOOPA_NORMAL_BBOX_HEIGHT / 2;
-		right = left + KOOPA_NORMAL_BBOX_WIDTH;
-		bottom = top + KOOPA_NORMAL_BBOX_HEIGHT;
-	}
-	else
-	{
-		left = x - KOOPA_SHELL_BBOX_WIDTH / 2;
-		top = y - KOOPA_SHELL_BBOX_HEIGHT / 2;
-		right = left + KOOPA_SHELL_BBOX_WIDTH;
-		bottom = top + KOOPA_SHELL_BBOX_HEIGHT;
-	}
 }
 
 
 void CKoopaRedNormal::Update(DWORD dt, vector<LPGAMEOBJECT>* coObjects)
 {
-	vy += ay * dt;
 	if (state == KOOPA_STATE_WALKING &&
-		attachedBBox != NULL && 
+		attachedBBox != NULL &&
 		attachedBBox->GetState() == ATTACHED_BBOX_STATE_FALL)
 	{
 		vx = -vx;
@@ -56,6 +31,9 @@ void CKoopaRedNormal::Update(DWORD dt, vector<LPGAMEOBJECT>* coObjects)
 		attachedBBox = new CAttachedBBox(x + nx * KOOPA_NORMAL_BBOX_WIDTH, y, vx, vy);
 		this->currentScene->AddObject(attachedBBox);
 	}
+
+	vy += ay * dt;
+
 
 	if (isShell && GetTickCount64() - shell_start > KOOPA_SHELL_TIMEOUT &&
 		state != KOOPA_STATE_SHELL_DOWNSIDE_MOVING &&
@@ -70,159 +48,38 @@ void CKoopaRedNormal::Update(DWORD dt, vector<LPGAMEOBJECT>* coObjects)
 		SetState(KOOPA_STATE_WALKING);
 	}
 			
+	CKoopa::Update(dt, coObjects);
 
-	CGameObject::Update(dt, coObjects);
-	CCollision::GetInstance()->Process(this, dt, coObjects);
 	//if (isBeingHeld)
 	//DebugOut(L"[INFO] Koopa x : %0.5f, koopa y : %0.5f, vx: %0.5f, vy :%0.5f \n", x, y, vx, vy);
-}
-
-
-void CKoopaRedNormal::OnNoCollision(DWORD dt)
-{
-	x += vx * dt;
-	y += vy * dt;
-};
-
-void CKoopaRedNormal::OnCollisionWith(LPCOLLISIONEVENT e)
-{
-	if (dynamic_cast<CKoopaRedNormal*>(e->obj)) return;
-	if (dynamic_cast<CMario*>(e->obj)) return;
-
-	if (e->ny != 0 && e->obj->IsBlocking())
-	{
-		vy = 0;
-
-	}
-	else if (e->nx != 0 && e->obj->IsBlocking())
-	{
-		vx = -vx;
-	}
-
-	if (dynamic_cast<CPlatformGhost*>(e->obj))
-		OnCollisionWithPlatformGhost(e);
-
-	else if (dynamic_cast<CGoomba*>(e->obj) || dynamic_cast<CGoombaRedWing*>(e->obj))
-		OnCollisionWithGoomba(e);
-
-	else if (dynamic_cast<CPlantRedFire*>(e->obj))
-		OnCollisionWithPlantRedFire(e);
-
-	else if (dynamic_cast<CBrickQuestionMark*>(e->obj))
-		OnCollisionWithBrickQuestionMark(e);
-
-	if (dynamic_cast<CDeadZone*>(e->obj))
-		this->Delete();
-}
-
-void CKoopaRedNormal::OnCollisionWithPlatformGhost(LPCOLLISIONEVENT e)
-{
-	if (e->ny < 0)
-	{
-
-		CPlatformGhost* platform = dynamic_cast<CPlatformGhost*>(e->obj);
-		float platform_l, platform_t, platform_r, platform_b;
-		platform->GetBoundingBox(platform_l, platform_t, platform_r, platform_b);
-
-		float koopa_l, koopa_t, koopa_r, koopa_b;
-		this->GetBoundingBox(koopa_l, koopa_t, koopa_r, koopa_b);
-
-		// Have to directly change koopa's y because ghost platform is non-blocking, so the 
-		// collisions with it are not handled by the framework (inside that, the coordinates
-		// of the src_obj that collided with blocking things are automatically adjusted
-		// to be the correct numbers using some calculations with BLOCK_PUSH_FACTOR)
-		y = platform_t - (koopa_b - koopa_t)/ 2 - BLOCK_PUSH_FACTOR_GHOST_PLATFORM;
-
-		vy = 0;
-	}
-}
-
-void CKoopaRedNormal::OnCollisionWithPlantRedFire(LPCOLLISIONEVENT e)
-{
-	if (state != KOOPA_STATE_SHELL_DOWNSIDE_MOVING && state != KOOPA_STATE_SHELL_UPSIDE_MOVING)
-		return;
-	CPlantRedFire* plant = dynamic_cast<CPlantRedFire*>(e->obj);
-	CGame::GetInstance()->UpdateScores(plant->GetScoresGivenWhenHit());
-	plant->Delete();
-}
-
-void CKoopaRedNormal::OnCollisionWithGoomba(LPCOLLISIONEVENT e)
-{
-	if (state != KOOPA_STATE_SHELL_DOWNSIDE_MOVING && state != KOOPA_STATE_SHELL_UPSIDE_MOVING)
-		return;
-	if (dynamic_cast<CGoomba*>(e->obj))
-	{
-		CGoomba* goomba = (dynamic_cast<CGoomba*>(e->obj));
-		if (goomba->GetState() != GOOMBA_STATE_DIE)
-		{
-			goomba->SetState(GOOMBA_STATE_DIE);
-			CGame::GetInstance()->UpdateScores(goomba->GetScoresGivenWhenHit());
-		}
-	}
-	else if (dynamic_cast<CGoombaRedWing*>(e->obj))
-	{
-		CGoombaRedWing* goomba = (dynamic_cast<CGoombaRedWing*>(e->obj));
-		if (goomba->GetState() != GOOMBA_RED_WING_STATE_DIE)
-		{
-			goomba->SetState(GOOMBA_RED_WING_STATE_DIE);
-			CGame::GetInstance()->UpdateScores(goomba->GetScoresGivenWhenHit());
-		}
-	}
-
-
-}
-
-void CKoopaRedNormal::OnCollisionWithBrickQuestionMark(LPCOLLISIONEVENT e)
-{
-	if (state != KOOPA_STATE_SHELL_DOWNSIDE_MOVING && state != KOOPA_STATE_SHELL_UPSIDE_MOVING)
-		return;
-	CBrickQuestionMark* brick = dynamic_cast<CBrickQuestionMark*>(e->obj);
-	if (brick->IsHidingItem())
-	{
-		float brick_x, brick_y;
-		brick->GetPosition(brick_x, brick_y);
-
-		CItem* hiddenItem;
-
-		// TODO: Need a better way than referencing like this
-		CMario* mario = (CMario*)this->currentScene->GetPlayer();
-		if (mario->GetLevel() == MARIO_LEVEL_SMALL)
-			hiddenItem = new CMushroomBig(brick_x, brick_y);
-		else
-			hiddenItem = new CLeaf(brick_x, brick_y);
-
-		brick->AddHiddenItem(hiddenItem);
-		this->currentScene->AddObject(hiddenItem);
-	}
-	e->obj->SetState(BRICK_STATE_HIT_BY_MARIO);
 }
 
 int CKoopaRedNormal::GetAniId()
 {
 	if (state == KOOPA_STATE_WALKING)
 	{
-		if (vx <= 0) return ID_ANI_KOOPA_WALKING_LEFT;
-		else return ID_ANI_KOOPA_WALKING_RIGHT;
+		if (vx <= 0) return ID_ANI_KOOPA_RED_NORMAL_WALKING_LEFT;
+		else return ID_ANI_KOOPA_RED_NORMAL_WALKING_RIGHT;
 	}
 
 	if (state == KOOPA_STATE_SHELL_DOWNSIDE_STILL ||
 		state == KOOPA_STATE_SHELL_DOWNSIDE_MARIO_HOLD)
-		return ID_ANI_KOOPA_SHELL_DOWNSIDE_STILL;
+		return ID_ANI_KOOPA_RED_NORMAL_SHELL_DOWNSIDE_STILL;
 
 	if (state == KOOPA_STATE_SHELL_UPSIDE_STILL ||
 		state == KOOPA_STATE_SHELL_UPSIDE_MARIO_HOLD)
-		return ID_ANI_KOOPA_SHELL_UPSIDE_STILL;
+		return ID_ANI_KOOPA_RED_NORMAL_SHELL_UPSIDE_STILL;
 
 	if (state == KOOPA_STATE_SHELL_DOWNSIDE_MOVING)
 	{
-		if (vx <= 0) return ID_ANI_KOOPA_SHELL_DOWNSIDE_MOVING_LEFT;
-		else return ID_ANI_KOOPA_SHELL_DOWNSIDE_MOVING_RIGHT;
+		if (vx <= 0) return ID_ANI_KOOPA_RED_NORMAL_SHELL_DOWNSIDE_MOVING_LEFT;
+		else return ID_ANI_KOOPA_RED_NORMAL_SHELL_DOWNSIDE_MOVING_RIGHT;
 	}
 
 	if (state == KOOPA_STATE_SHELL_UPSIDE_MOVING)
 	{
-		if (vx <= 0) return ID_ANI_KOOPA_SHELL_UPSIDE_MOVING_LEFT;
-		else return ID_ANI_KOOPA_SHELL_UPSIDE_MOVING_RIGHT;
+		if (vx <= 0) return ID_ANI_KOOPA_RED_NORMAL_SHELL_UPSIDE_MOVING_LEFT;
+		else return ID_ANI_KOOPA_RED_NORMAL_SHELL_UPSIDE_MOVING_RIGHT;
 	}
 
 	return -1;
@@ -243,131 +100,3 @@ void CKoopaRedNormal::Render()
 	//DebugOut(L"[INFO] Koopa's Render() has been called \n");
 }
 
-void CKoopaRedNormal::SetState(int state)
-{
-	CGameObject::SetState(state);
-	switch (state)
-	{
-		case KOOPA_STATE_WALKING:
-		{
-			isShell = false;
-			vx = nx * KOOPA_WALKING_SPEED;
-			ay = KOOPA_GRAVITY;
-
-			// To keep koopa on the platform after turning from shell to normal (just like with
-			// mario_big to mario_small)
-			y -= (KOOPA_NORMAL_BBOX_HEIGHT - KOOPA_SHELL_BBOX_HEIGHT);
-
-			if (attachedBBox == NULL)
-			{
-				attachedBBox = new CAttachedBBox(x + nx * KOOPA_NORMAL_BBOX_WIDTH, y, vx, vy);
-				this->currentScene->AddObject(attachedBBox);
-			}
-			break;
-		}
-
-		case KOOPA_STATE_SHELL_DOWNSIDE_STILL:
-		{
-			isShell = true;
-			shell_start = GetTickCount64();
-			vx = 0;
-
-			if (attachedBBox != NULL)
-			{
-				attachedBBox->Delete();
-				attachedBBox = NULL;
-			}
-			break;
-		}
-
-		case KOOPA_STATE_SHELL_DOWNSIDE_MARIO_HOLD:
-		{
-			isBeingHeld = true;
-			vx = 0;
-			vy = 0;
-			ay = 0;
-
-			if (attachedBBox != NULL)
-			{
-				attachedBBox->Delete();
-				attachedBBox = NULL;
-			}
-			break;
-		}
-
-		case KOOPA_STATE_SHELL_DOWNSIDE_MOVING:
-		{
-			isBeingHeld = false;
-			y -= (KOOPA_NORMAL_BBOX_HEIGHT - KOOPA_SHELL_BBOX_HEIGHT);
-			vx = nx * KOOPA_SHELL_MOVING_SPEED;
-			ay = KOOPA_GRAVITY;
-
-			if (attachedBBox != NULL)
-			{
-				attachedBBox->Delete();
-				attachedBBox = NULL;
-			}
-			break;
-		}
-
-		case KOOPA_STATE_SHELL_UPSIDE_STILL:
-		{
-			isShell = true;
-			shell_start = GetTickCount64();
-			vx = 0;
-
-			if (attachedBBox != NULL)
-			{
-				attachedBBox->Delete();
-				attachedBBox = NULL;
-			}
-			break;
-		}
-
-		case KOOPA_STATE_SHELL_UPSIDE_MARIO_HOLD:
-		{
-			isBeingHeld = true;
-			vx = 0;
-			vy = 0;
-			ay = 0;
-
-			if (attachedBBox != NULL)
-			{
-				attachedBBox->Delete();
-				attachedBBox = NULL;
-			}
-			break;
-		}
-
-		case KOOPA_STATE_SHELL_UPSIDE_MOVING:
-		{
-			isBeingHeld = false;
-			y -= (KOOPA_NORMAL_BBOX_HEIGHT - KOOPA_SHELL_BBOX_HEIGHT);
-			vx = nx * KOOPA_SHELL_MOVING_SPEED;
-			ay = KOOPA_GRAVITY;
-
-			if (attachedBBox != NULL)
-			{
-				attachedBBox->Delete();
-				attachedBBox = NULL;
-			}
-			break;
-		}
-
-		case KOOPA_STATE_DIE:
-		{
-			if (attachedBBox != NULL)
-			{
-				attachedBBox->Delete();
-				attachedBBox = NULL;
-			}
-			vx = 0;
-			vy = 0;
-			ay = 0;
-			isDeleted = true;
-			break;
-		}
-			
-
-	}
-}
