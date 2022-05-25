c459f84e612b09d839b03f5f3d018e9499e4a934
Doan Pham <Doan-Pham@users.noreply.github.com>
2022-05-25 22:17:48 +0700

    Draw a line of text onto successfully with CText class
    This is done by loading an object from Tiled Edit into game, this object holds the text. Then the text is cast to vector of chars and every single char is then rendered in turn

--- SuperMarioBros3/Text.cpp
+++ SuperMarioBros3/Text.cpp
@@ -1,1 +1,48 @@
 #include "Text.h"
+#include "debug.h"
+#include "Sprites.h"
+
+void CText::Render()
+{
+	if (characters.size() < 1) return;
+	int spriteId = -1;
+	float single_char_x = x;
+	for (char character : characters)
+	{
+		spriteId = getSpriteId(character);
+		if (spriteId == -1)
+		{
+			DebugOut(L"[ERROR] Can't find sprite id for character %c - ascii code : %i", 
+				character, character);
+			return;
+		}
+
+		// Skip if character is blank space
+		else if (spriteId != 999)
+			CSprites::GetInstance()->Get(spriteId)->Draw(single_char_x, y);
+		
+		single_char_x += CHARACTER_STANDARD_WIDTH;
+	}
+}
+
+int CText::getSpriteId(char character) 
+{
+	int spriteId = -1;
+	if (character == ASCII_CODE_CHARACTER_SPACE)
+	{
+		spriteId = 999;
+	}
+	if (character >= ASCII_CODE_CHARACTER_0 && character <= ASCII_CODE_CHARACTER_9)
+	{
+		spriteId = ID_SPRITE_CHARACTER_0 + (character - ASCII_CODE_CHARACTER_0);
+	}
+	else if (character >= ASCII_CODE_CHARACTER_A && character <= ASCII_CODE_CHARACTER_Z)
+	{
+		spriteId = ID_SPRITE_CHARACTER_A + (character - ASCII_CODE_CHARACTER_A);
+	}
+	else if (character >= ASCII_CODE_CHARACTER_a && character <= ASCII_CODE_CHARACTER_z)
+	{
+		spriteId = ID_SPRITE_CHARACTER_A + (character - ASCII_CODE_CHARACTER_a);
+	}
+	return spriteId;
+}
--- SuperMarioBros3/Text.h
+++ SuperMarioBros3/Text.h
@@ -1,6 +1,29 @@
 #pragma once
 
 #include "GameObject.h"
+
+#define CHARACTER_STANDARD_WIDTH	8
+#define CHARACTER_STANDARD_HEIGHT	8
+
+#define ASCII_CODE_CHARACTER_SPACE	32
+
+#define ASCII_CODE_CHARACTER_0		48
+#define ASCII_CODE_CHARACTER_9		57
+
+#define ASCII_CODE_CHARACTER_A		65
+#define ASCII_CODE_CHARACTER_Z		90
+
+#define ASCII_CODE_CHARACTER_a		97
+#define ASCII_CODE_CHARACTER_z		122
+
+#define ID_SPRITE_CHARACTER_0		82001
+#define ID_SPRITE_CHARACTER_9		82010
+
+#define ID_SPRITE_CHARACTER_A		82011
+#define ID_SPRITE_CHARACTER_Z		82036
+
+
+// A class to draw text onto screen
 class CText : public CGameObject
 {
 protected:
@@ -10,7 +33,9 @@ public:
 	{
 		this->characters = characters;
 	}
-	void Render() {};
+	int IsBlocking() { return 0; }
+	int getSpriteId(char character);
+	void Render();
 	void GetBoundingBox(float& left, float& top, float& right, float& bottom) {};
 };
 
