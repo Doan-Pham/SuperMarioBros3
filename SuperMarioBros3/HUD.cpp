1baf458239ceabd43ec4f1643d7347de3b76ca1b
Doan Pham <Doan-Pham@users.noreply.github.com>
2022-05-26 20:00:14 +0700

    Fix bug - bottom HUD shakes when camera follows mario on x-axis
    Cause: The HUD's x, y coordinates before rendering are set to (coord + cam_coord) to make sure it's independent of camera coordinates. However this combination causes some slight mismatch when being rounded later in Sprites::Draw() method
    
    Solution: Use floor() method first on the HUD's coords and cam's coords before even going into Sprites::Draw()

--- SuperMarioBros3/HUD.cpp
+++ SuperMarioBros3/HUD.cpp
@@ -2,6 +2,7 @@
 #include "Sprites.h"
 #include "Game.h"
 #include "debug.h"
+#include <string>
 
 void CHUD::GetBoundingBox(float& left, float& top, float& right, float& bottom)
 {
@@ -11,22 +12,37 @@ void CHUD::GetBoundingBox(float& left, float& top, float& right, float& bottom)
 	bottom = top + height;
 }
 
+void CHUD::Update(DWORD dt)
+{
+	float cam_x, cam_y;
+	CGame::GetInstance()->GetCamPos(cam_x, cam_y);
+
+	scores_text->SetPosition(
+		x + HUD_SCORES_POSITION_X_OFFSET_CENTER + cam_x,
+		y + HUD_SCORES_POSITION_Y_OFFSET_CENTER + cam_y);
+
+	time_text->SetPosition(
+		x + HUD_TIME_POSITION_X_OFFSET_CENTER + cam_x,
+		y + HUD_TIME_POSITION_Y_OFFSET_CENTER + cam_y);
+}
+
 void CHUD::Render()
 {
 	float cam_x, cam_y;
 	CGame::GetInstance()->GetCamPos(cam_x, cam_y);
+
+	// We need to use the floor() method on these coordinates first or else inside the Sprites::Draw() 
+	// method later, the flooring then will make coordinates and create shaking when rendering 
+	cam_x = (FLOAT)floor(cam_x);
+	cam_y = (FLOAT)floor(cam_y);
+
+	x = (FLOAT)floor(x);
+	y = (FLOAT)floor(y);
+
 	CSprites::GetInstance()->Get(ID_SPRITE_HUD_BOTTOM_FRAME)->Draw(x + cam_x , y + cam_y);
 
-	scores->SetPosition(
-		x + HUD_SCORES_POSITION_X_OFFSET_CENTER + cam_x, 
-		y + HUD_SCORES_POSITION_Y_OFFSET_CENTER + cam_y);
+	scores_text->Render();
+	time_text->Render();
 
-	scores->Render();
-
-	time->SetPosition(
-		x + HUD_TIME_POSITION_X_OFFSET_CENTER + cam_x,
-		y + HUD_TIME_POSITION_Y_OFFSET_CENTER + cam_y);
-
-	time->Render();
 	DebugOut(L"hud_x : %0.5f; hud_y : %0.5f, cam_x : %0.5f, cam_y : %0.5f \n", x, y, cam_x, cam_y);
 }
