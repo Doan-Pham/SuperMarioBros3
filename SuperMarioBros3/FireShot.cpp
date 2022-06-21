be7d54a6644d1d9fd3afcffd02d4f1fef5c7c317
Doan Pham <Doan-Pham@users.noreply.github.com>
2022-06-21 13:53:21 +0700

    Delete plants' fireshots when they go out of camera range

--- SuperMarioBros3/FireShot.cpp
+++ SuperMarioBros3/FireShot.cpp
@@ -5,16 +5,16 @@ CFireShot::CFireShot(float x, float y, int destination_x, int destination_y)
 {
 	this->destination_x = destination_x;
 	this->destination_y = destination_y;
-	this->nx = (destination_x - x ) / abs(destination_x - x);
+	this->nx = (destination_x - x) / abs(destination_x - x);
 	this->ny = (destination_y - y) / abs(destination_y - y);
 
 	// This vx adjustment keeps the time fireshot takes to reach mario the same
 	// TODO: This is not how it goes in the original game, however it's because in our framework, vx vy
 	// presents both velocity and direction
-	vx = nx * abs(destination_x - x) / FIRESHOT_STANDARD_TIME_X ;
+	vx = nx * abs(destination_x - x) / FIRESHOT_STANDARD_TIME_X;
 
 	// vy is adjusted based on vx to create the desired shooting angle
-	vy = ny * abs(vx) * abs((destination_y - y)/(destination_x - x));
+	vy = ny * abs(vx) * abs((destination_y - y) / (destination_x - x));
 }
 
 void CFireShot::Render()
@@ -26,6 +26,12 @@ void CFireShot::Update(DWORD dt, vector<LPGAMEOBJECT>* coObjects)
 {
 	x += vx * dt;
 	y += vy * dt;
+	float cam_x, cam_y;
+	CGame::GetInstance()->GetCamPos(cam_x, cam_y);
+
+	if ((x < cam_x) || (x > (cam_x + SCREEN_WIDTH)) ||
+		(y < cam_y) || (y > (cam_y + SCREEN_HEIGHT)))
+		this->Delete();
 }
 
 void CFireShot::GetBoundingBox(float& l, float& t, float& r, float& b)
